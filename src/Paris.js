8b1741ad21456b3eb4792b637a51394e9f94192c
az02754 <clement.c.malarme@renault.com>
2021-02-19 10:19:21 +0100

    modal ok

--- src/Paris.js
+++ src/Paris.js
@@ -3,17 +3,28 @@ import FormulaireAjout from "./FormulaireAjout";
 import Alert from "./Alert"
 import axios from "axios";
 import Bouton from "./Bouton";
+import {Button, Modal} from "react-bootstrap";
+
 
 class Paris extends Component {
-    state = {
-        bets: [],
-        alertMessage: null
+
+    constructor(props) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+
+        this.state = {
+            bets: [],
+            alertMessage: null,
+            showModal: false
+        }
     }
 
-    handleSuppressionPari= (event,data) => {
-        console.log(data)
-        console.log("bouton qui marche")
-        axios.delete('http://localhost:8080/bets/'+ data)
+    handleSuppressionPari = (event, data) => {
+        console.log(data);
+        console.log("bouton qui marche");
+        axios.delete('http://localhost:8080/bets/' + data)
             .then((res) => {
                 this.reloadBets()
             })
@@ -38,27 +49,42 @@ class Paris extends Component {
         //
         //     }, 5000)
         // });
-    }
+    };
+
+    handleModifyBet = (e) => {
+        e.preventDefault();
+        console.log("bouton qui marche");
+        this.setState({showModal: true})
+    };
 
 
-    reloadBets = ()=> {
+    reloadBets = () => {
         axios.get('http://localhost:8080/bets')
             .then((res) => {
-                console.log(res)
+                console.log(res);
                 this.setState({bets: res.data})
             }).catch((error) => {
             console.log(error)
         });
-    }
+    };
 
     componentDidMount() {
         axios.get('http://localhost:8080/bets')
             .then((res) => {
-                console.log(res)
+                console.log(res);
                 this.setState({bets: res.data})
             }).catch((error) => {
             console.log(error)
         });
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        this.setState({showModal: true})
+    }
+
+    handleClose() {
+        this.setState({showModal: false})
     }
 
 
@@ -72,11 +98,26 @@ class Paris extends Component {
                     <td>{bet.stake}</td>
                     <td>{bet.winner}</td>
                     <td>{bet.endbet}</td>
-                    <td><Bouton typeBtn={"btn-danger"} onClick={(event)=>this.handleSuppressionPari(event,bet.id)} value={bet.id}>delete</Bouton></td>
+                    <td><Bouton typeBtn={"btn-danger"} onClick={(event) => this.handleSuppressionPari(event, bet.id)}
+                                value={bet.id}>delete</Bouton></td>
+                    <td><Bouton typeBtn={"btn-success"} onClick={this.handleClick}>Modify</Bouton></td>
                 </tr>)
-        })
+        });
         return (
             <>
+                <Modal show={this.state.showModal}>
+                    <Modal.Header closeButton onClick={this.handleClose}>
+                        <Modal.Title>modifier date pari</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <p>Bienvenue sur BetFriend</p>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button variant="success" onClick={this.handleClose}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
                 {this.state.alertMessage &&
                 <Alert typeAlert={this.state.alertMessage.type}>{this.state.alertMessage.message}</Alert>}
                 <table className="table table-hover">
@@ -89,6 +130,7 @@ class Paris extends Component {
                         <th scope="col">winner</th>
                         <th scope="col">endbet</th>
                         <th scope="col">delete</th>
+                        <th scope="col">modify</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -97,7 +139,7 @@ class Paris extends Component {
                 </table>
                 {this.props.ajoutPari && <FormulaireAjout reloadBets={this.reloadBets}/>}
             </>
-        )
+        );
     }
 }
 
